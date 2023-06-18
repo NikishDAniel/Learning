cc8fb00c65479ae5e300f98b99c262210014e7af
NikishDAniel <nikishdaniel77@gmail.com>
2023-06-18 12:53:29 +0530

    updated

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 print("hello world")
 #printing hello world
 #hi fayas and barath and nikish
+
+first = 'nikish'
+last = 'daniel'
+print(first,last,sep=' - ')
--- new3.py
+++ new3.py
@@ -1,9 +1,15 @@
-number = input("Enter your phone number :")
-if len(number) == 10:
-    for i in number:
-        if i.isnumeric():
-            print("valid")
-        else:
-            print("not valid")
+number = input("Enter your phone number :").replace("",' ').split()
+'''result = list(x for x in number if x.isnumeric())
+if len(result) == 10:
+    print('true')
 else:
-    print("try to add more digits")
+    print('false')
+    
+    '''
+'''result = [1,2,3,4,5,6,7]
+print(list(filter(lambda x:x%2 == 0,result)))'''
+result = list(filter(lambda x : x.isnumeric(),number))
+if len(result) == 10:
+    print('true')
+else:
+    print('false')
